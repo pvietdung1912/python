a4df0387d642eadbc31549750c40c6f8d9ea9d28
pvietdung1912 <150446316+pvietdung1912@users.noreply.github.com>
2024-01-20 19:06:34 +0700

    Create bt2.py

---
+++ bt2.py
@@ -0,0 +1,6 @@
+import random
+import statistics
+mean=statistics.mean([0.3,0.4,0.6,0.9,0.12,0.22])
+print('"gia tri trung binh la:',mean)
+median=statistics.median([0.3,0.4,0.6,0.9,0.12,0.22])
+print('gia tri trung vi la:',median)
