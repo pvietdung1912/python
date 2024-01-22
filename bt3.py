921349201834c20537f1c2272d1d7a1c40667b21
pvietdung1912 <150446316+pvietdung1912@users.noreply.github.com>
2024-01-22 17:15:55 +0700

    Add files via upload

---
+++ bt3.py
@@ -0,0 +1,14 @@
+ds=[0.3,0.4,0.6,0.9,0.12,0.22]
+def mean(ds):
+	tb=sum(list_3)/len(list_3)
+	return tb;
+tb=mean([0.3,0.4,0.6,0.9,0.12,0.22])
+print(tb)
+def median(ds):
+	if ((len(ds)%2)==0)
+		tv=(ds[len(ds)//2-1]+ds[len(ds)//2+1])
+	else 
+		tv=ds[len(ds)//2]
+	return tv;
+tv=median(ds)
+print(tv)
